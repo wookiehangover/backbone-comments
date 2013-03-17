3e6990016889b32413b42c95605513db27eab111
Sam Breed <sam@quickleft.com>
2013-03-17 16:23:17 -0600

    adding markdown rendering for comments

--- app/config.js
+++ app/config.js
@@ -6,8 +6,16 @@ require.config({
     jquery: '../components/jquery/jquery',
     underscore: '../components/lodash/lodash',
     backbone: '../components/backbone-amd/backbone',
+    markdown: '../components/markdown/lib/markdown',
+    moment: '../components/moment/moment',
     tpl: '../components/requirejs-tpl/tpl',
     'backbone.localStorage': '../components/backbone.localStorage/backbone.localStorage'
+  },
+
+  shim: {
+    markdown: {
+      exports: 'markdown'
+    }
   }
 
 });
--- app/views/comment-view.js
+++ app/views/comment-view.js
@@ -1,10 +1,15 @@
 define(function(require, exports, module){
 
+  var _ = require('underscore');
   var Backbone = require('backbone');
+  var markdown = require('markdown');
+  var moment = require('moment');
 
   module.exports = Backbone.View.extend({
 
     tagName: 'li',
+
+    timer_refresh: 60e3,
 
     template: require('tpl!templates/comment.ejs'),
 
@@ -12,11 +17,40 @@ define(function(require, exports, module){
       if( !this.model ){
         throw new Error('You must provide a Comment model');
       }
+
+      this.listenTo( this.model, 'remove', this.remove);
+
+      _.bindAll(this, 'updateTime');
+      this.timer = setTimeout(this.updateTime, this.timer_refresh);
     },
 
     render: function(){
       this.$el.html( this.template( this ) );
-      return this.el;
+      return this.$el;
+    },
+
+    formatDate: function(){
+      var date = moment( this.model.get('created_at') );
+      return date.fromNow();
+    },
+
+    renderMarkdown: function(){
+      return markdown.toHTML( this.model.get('content') );
+    },
+
+    updateTime: function(){
+      this.$('.date').text( this.formatDate() );
+      this.timer = setTimeout(this.updateTime, this.timer_refresh);
+    },
+
+    // Overload remove to clean up the timer, if it's running
+    remove: function(){
+      if( this.timer ){
+        clearTimeout( this.timer );
+      }
+      this.$el.slideUp(_.bind(function(){
+        Backbone.View.prototype.remove.call(this);
+      }, this));
     }
 
   });
