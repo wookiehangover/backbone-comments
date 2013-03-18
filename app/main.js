b729581034e62e0140027c661bb69abe6b1cf964
Sam Breed <sam@quickleft.com>
2013-03-17 21:32:41 -0600

    adding grunt

--- app/main.js
+++ app/main.js
@@ -1,7 +1,7 @@
 define(function(require, exports, module){
 
   var $ = require('jquery');
-  var App = require('./app');
+  var App = require('app');
 
   $(function(){
     window.comments = new App();
